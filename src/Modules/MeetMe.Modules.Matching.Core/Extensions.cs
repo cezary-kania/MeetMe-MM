0c0cecb3e9fa7bedaae797060b032d49f768234a
cezary-kania <cezary.kaniadev@gmail.com>
2023-04-17 21:47:48 +0200

    Add missing matching project files

---
+++ src/Modules/MeetMe.Modules.Matching.Core/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using MeetMe.Modules.Matching.Core.DAL;
+using MeetMe.Modules.Matching.Core.Services;
+using MeetMe.Shared.Database;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeetMe.Modules.Matching.Core;
+
+public static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        services.AddPostgres<MatchingDbContext>();
+        services.AddScoped<IUserMatchService, UserMatchService>();
+        services.AddScoped<IDecisionResultStore, HttpContextDecisionResultStore>();
+        return services;
+    }
+}
