a740d3ec115aa5a90558596f1b1015fecdc7a624
cezary-kania <cezary.kaniadev@gmail.com>
2023-04-26 20:37:10 +0200

    Add modules migrations

---
+++ src/Modules/MeetMe.Modules.Matching.Core/DAL/Migrations/20230426181854_Init_Matching.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetMe.Modules.Matching.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init_Matching : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "matching");
+
+            migrationBuilder.CreateTable(
+                name: "Decisions",
+                schema: "matching",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProfileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DecisionType = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Decisions", x => new { x.ProfileId, x.UserId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filters",
+                schema: "matching",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MinAge = table.Column<long>(type: "bigint", maxLength: 5, nullable: false),
+                    MaxAge = table.Column<long>(type: "bigint", maxLength: 5, nullable: false),
+                    Gender = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filters", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                schema: "matching",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    User1Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    User2Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                schema: "matching",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Age = table.Column<long>(type: "bigint", maxLength: 5, nullable: false),
+                    Gender = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Decisions",
+                schema: "matching");
+
+            migrationBuilder.DropTable(
+                name: "Filters",
+                schema: "matching");
+
+            migrationBuilder.DropTable(
+                name: "Matches",
+                schema: "matching");
+
+            migrationBuilder.DropTable(
+                name: "Profiles",
+                schema: "matching");
+        }
+    }
+}
---
+++ src/Modules/Profiles/MeetMe.Modules.Profiles.Core/DAL/Migrations/20230404174106_Init_Profiles.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetMe.Modules.Profiles.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init_Profiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "profiles");
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                schema: "profiles",
+                columns: table => new
+                {
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Age = table.Column<long>(type: "bigint", nullable: true),
+                    Gender = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.OwnerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interests",
+                schema: "profiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ProfileOwnerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Interests_Profiles_ProfileOwnerId",
+                        column: x => x.ProfileOwnerId,
+                        principalSchema: "profiles",
+                        principalTable: "Profiles",
+                        principalColumn: "OwnerId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProfileImages",
+                schema: "profiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProfileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DisplayOrder = table.Column<long>(type: "bigint", maxLength: 100, nullable: false),
+                    BinaryData = table.Column<byte[]>(type: "bytea", nullable: false),
+                    ProfileOwnerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfileImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProfileImages_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalSchema: "profiles",
+                        principalTable: "Profiles",
+                        principalColumn: "OwnerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProfileImages_Profiles_ProfileOwnerId",
+                        column: x => x.ProfileOwnerId,
+                        principalSchema: "profiles",
+                        principalTable: "Profiles",
+                        principalColumn: "OwnerId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interests_ProfileOwnerId",
+                schema: "profiles",
+                table: "Interests",
+                column: "ProfileOwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileImages_ProfileId",
+                schema: "profiles",
+                table: "ProfileImages",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileImages_ProfileOwnerId",
+                schema: "profiles",
+                table: "ProfileImages",
+                column: "ProfileOwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interests",
+                schema: "profiles");
+
+            migrationBuilder.DropTable(
+                name: "ProfileImages",
+                schema: "profiles");
+
+            migrationBuilder.DropTable(
+                name: "Profiles",
+                schema: "profiles");
+        }
+    }
+}
